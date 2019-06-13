b5c673741f81ce57f3b9a564cd3f624d1b6d3b44
Onyi Cho <onyicho@gmail.com>
2019-06-13 14:32:37 -0700

    Add monkey patch Array#twoSum

--- js_intro/phase_1_arrays.js
+++ js_intro/phase_1_arrays.js
@@ -1,9 +1,21 @@
 Array.prototype.uniq = function() {
-  console.log(this);
-  arr = [];
+  let arr = [];
   this.forEach(function(ele) {
     if (!arr.includes(ele)){
       arr.push(ele);
+    }
+  });
+  return arr;
+}
+
+Array.prototype.twoSum = function () {
+  let arr = [];
+  let base_arr = this;
+  this.forEach(function (ele, idx) {
+    for (let i = idx+1; i < base_arr.length; i++) {
+      if ( (ele + base_arr[i]) === 0){
+        arr.push([idx, i]);
+      }
     }
   });
   return arr;
