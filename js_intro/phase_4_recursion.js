37672364d934733539566f2dbdb8801750fdd50b
Onyi Cho <onyicho@gmail.com>
2019-06-13 16:17:17 -0700

    Add Phase 4 recursive sumRec and exponent method

--- js_intro/phase_4_recursion.js
+++ js_intro/phase_4_recursion.js
@@ -8,3 +8,28 @@ function range(start, end){
   }
   return [].concat(start, range(start + 1, end));
 }
+
+function sumRec(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.pop() + sumRec(arr);
+}
+
+function exponent1(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  return base * exponent1(base, exp - 1);
+}
+
+function exponent2(base, exp) {
+  if (exp === 0) return 1;
+  if (exp === 1) return base;
+    
+  if (exp % 2 === 0) {
+    return exponent2(base, exp / 2) ** 2;
+  } else {
+    return base * (exponent2(base, (exp - 1) / 2) ** 2);    
+  }
+}
